de21369e59cbc0c4ac6114a7a9abe036566fa822
Kevin <desca5b@gmail.com>
2024-10-15 00:31:57 -0300

    Se arreglo problema de conexión con la DB

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
+/* Este código se lo proporcionó Prisma a un usuario, para no estar iterando sobre varios PrismaClient, lo cual baja el rendimiento a tu aplicación */
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
